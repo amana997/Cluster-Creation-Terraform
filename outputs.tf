34b6947e010585fe57867e402293fb42ff8baace
Aman Agrawal <aman@Amans-MacBook-Air.local>
2025-07-17 19:53:45 +0530

    CodeChef

---
+++ outputs.tf
@@ -0,0 +1,12 @@
+output "cluster_name" {
+  value = module.eks.cluster_name
+}
+
+output "kubeconfig" {
+  value = module.eks.kubeconfig
+}
+
+output "cluster_endpoint" {
+  value = module.eks.cluster_endpoint
+}
+
