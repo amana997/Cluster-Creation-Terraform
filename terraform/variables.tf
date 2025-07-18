fe153ccbb4db67532880057a4d539486dcb03741
Aman Agrawal <aman@Amans-MacBook-Air.local>
2025-07-18 20:07:51 +0530

    Terraform

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,3 +1,16 @@
 variable "region" {
-  default = "us-east-1"
+  default = "ap-south-1"
+}
+
+variable "instance_count" {
+  default = 3
+}
+
+variable "instance_type" {
+  default = "t2.micro"
+}
+
+variable "ami_id" {
+  description = "Amazon Linux 2 AMI"
+  default     = "ami-0c2b8ca1dad447f8a" # Update based on region
 }
