9044b6c710aa773a730ef9d0dbfa2b2ded62368b
Aman Agrawal <aman@Amans-MacBook-Air.local>
2025-07-19 13:24:55 +0530

    Terraform

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,36 +1,16 @@
-terraform {
-  required_providers {
-    aws = {
-      source  = "hashicorp/aws"
-      version = "~> 5.0"
-    }
-  }
-
-  required_version = ">= 1.3.0"
-}
-
-provider "aws" {
-  region = var.aws_region
-}
-
 module "vpc" {
   source  = "terraform-aws-modules/vpc/aws"
-  version = "5.1.1"
+  version = "5.1.0"
 
   name = "eks-vpc"
   cidr = "10.0.0.0/16"
 
-  azs             = ["${var.aws_region}a", "${var.aws_region}b"]
-  private_subnets = ["10.0.1.0/24", "10.0.2.0/24"]
-  public_subnets  = ["10.0.101.0/24", "10.0.102.0/24"]
+  azs             = ["${var.region}a", "${var.region}b", "${var.region}c"]
+  public_subnets  = ["10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24"]
+  private_subnets = ["10.0.11.0/24", "10.0.12.0/24", "10.0.13.0/24"]
 
-  enable_nat_gateway = true
-  single_nat_gateway = true
-
-  tags = {
-    Terraform   = "true"
-    Environment = "dev"
-  }
+  enable_dns_hostnames = true
+  enable_dns_support   = true
 }
 
 module "eks" {
@@ -38,25 +18,21 @@ module "eks" {
   version = "20.8.4"
 
   cluster_name    = var.cluster_name
-  cluster_version = "1.29"
-  subnet_ids      = module.vpc.private_subnets
-  vpc_id          = module.vpc.vpc_id
+  cluster_version = "1.33"
+  
+  cluster_endpoint_public_access  = true
+  cluster_endpoint_private_access = true
 
-  enable_irsa = true
+  subnet_ids         = module.vpc.private_subnets
+  vpc_id             = module.vpc.vpc_id
+  enable_irsa        = true
 
   eks_managed_node_groups = {
     default = {
-      desired_size = 2
-      max_size     = 3
-      min_size     = 1
-
-      instance_types = ["t3.medium"]
-      capacity_type  = "ON_DEMAND"
+      instance_types = [var.node_instance_type]
+      desired_size   = var.desired_capacity
+      min_size       = 1
+      max_size       = 3
     }
-  }
-
-  tags = {
-    Environment = "dev"
-    Terraform   = "true"
   }
 }
--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,7 +1,15 @@
-variable "aws_region" {
+variable "region" {
   default = "ap-south-1"
 }
 
 variable "cluster_name" {
   default = "my-eks-cluster"
+}
+
+variable "node_instance_type" {
+  default = "t2.micro"
+}
+
+variable "desired_capacity" {
+  default = 2
 }
