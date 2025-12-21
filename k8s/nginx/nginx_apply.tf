3fdcfe32d866714f471570b542a4316a082da298
Aman Agrawal <aman@Amans-MacBook-Air.local>
2025-12-21 14:31:09 +0530

    CodeChef

---
+++ k8s/nginx/nginx_apply.tf
@@ -0,0 +1,29 @@
+resource "null_resource" "apply_config" {
+  provisioner "local-exec" {
+    when    = create
+    command = <<EOT
+      aws eks update-kubeconfig --region ap-south-1 --name my-eks-cluster
+      sleep 10
+      kubectl apply -f /Users/aman/Documents/Git/Cluster-Creation-Terraform/k8s/nginx-deployment.yaml
+      sleep 10
+      kubectl get nodes
+      sleep 10
+      kubectl get pods -A
+      sleep 10
+      open "http://$(kubectl get svc | grep ".com" | awk '{print $4}' | tr -d '\n')"
+    EOT
+  }
+  provisioner "local-exec" {
+    when    = destroy
+    command = <<EOT
+      kubectl delete -f /Users/aman/Documents/Git/Cluster-Creation-Terraform/k8s/nginx-deployment.yaml
+      sleep 10
+    EOT
+  }
+
+  # Update this with your actual EKS cluster resource name
+  depends_on = [
+    module.eks,
+    kubernetes_namespace.world
+  ]
+}
